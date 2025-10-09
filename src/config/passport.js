f0ab41a9ec8ed8d973401377b023e65e0376002e
Satria Ibnu <satria0ibnu@gmail.com>
2025-10-09 10:51:54 +0700

    fix username required via google login

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,8 +1,10 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../models/User.js";
+import { nanoid } from "nanoid";
+import slugify from "slugify";
 
-console.log("Google confid loaded:", {
+console.log("Google config loaded:", {
   clientId: !!process.env.GOOGLE_CLIENT_ID,
   clientSecret: !!process.env.GOOGLE_CLIENT_SECRET,
   callbackUrl: !!process.env.GOOGLE_CALLBACK_URL,
@@ -17,14 +19,37 @@ passport.use(
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
-        let user = await User.findOne({ googleId: profile.id });
+        // Try to find existing user by Google ID or email
+        let user = await User.findOne({
+          $or: [
+            { googleId: profile.id },
+            { email: profile.emails?.[0]?.value },
+          ],
+        });
+
         if (!user) {
+          // Generate base username from Google displayName or givenName
+          const base = slugify(
+            profile.displayName || profile.name?.givenName || "user",
+            { lower: true, strict: true }
+          );
+
+          // Append nanoid to ensure uniqueness
+          let username = `${base}-${nanoid(6)}`;
+
+          // Double-check uniqueness in DB
+          while (await User.exists({ username })) {
+            username = `${base}-${nanoid(4)}`;
+          }
+
+          // Create new user
           user = await User.create({
             googleId: profile.id,
-            name: profile.displayName,
             email: profile.emails?.[0]?.value,
+            username,
           });
         }
+
         return done(null, user);
       } catch (err) {
         return done(err, null);
